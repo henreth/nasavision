51048241586626b6e6be8d546d0c06e54fa94c06
Henry Miller <mail.henrym+git@gmail.com>
2022-06-01 23:39:58 -0400

    new photo and title styling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './HomePage.css';
 import apiKey from '../../Apikey';
+import Photo from '../Photo/Photo';
 import axios from 'axios';
 
 let nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey
@@ -18,6 +19,7 @@ export default function HomePage(){
     return(
         <React.Fragment>
             <h1 className='title'>NASAVISION</h1>
+            <Photo data={photoOfTheDay} />
             <div className='background-container'>
                 <div class="stars"></div>
                 <div class="twinkling"></div>
--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,9 +1,10 @@
 import "./Photo.css"
 
-export default function Photo({photoOfTheDay}){
+export default function Photo({data}){
     return (
         <div className="photo-container">
-            
+            <img className='photoOftheDay' src={data.hdurl} />
+            <div className="photo-Title">{data.title}</div>
         </div>
     )
 }

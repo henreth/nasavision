15645678978dbc6ce1b62152e2c2f9beeccbf318
Henry Miller <mail.henrym+git@gmail.com>
2022-06-02 15:26:24 -0400

    add description to photo on click of info button

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,6 +1,7 @@
 import "./Photo.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleQuestion } from "@fortawesome/free-regular-svg-icons"
+import ReactTooltip from "react-tooltip";
 
 
 export default function Photo({data}){
@@ -10,10 +11,15 @@ export default function Photo({data}){
             <div className="photo-line-1">
                 <div className="photo-title">{data.title}</div>
                 <div className="photo-more">
-                    <FontAwesomeIcon icon={faCircleQuestion} />
+                    <FontAwesomeIcon icon={faCircleQuestion} data-tip data-event='click focus' />
                 </div>
             </div>
             <div className="photo-copyright">{data.copyright}</div>
+
+            <ReactTooltip  globalEventOff='click'> 
+                <h2>Description:</h2>
+                <span className="photo-information" >{data.explanation}</span>
+            </ReactTooltip>
         </div>
     )
 }

637d49ee85bd3b0291dbbea6aae56d9d5105a2a5
Henry Miller <mail.henrym+git@gmail.com>
2022-06-02 00:05:14 -0400

    add copyright and more info button (no functionality)

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,10 +1,19 @@
 import "./Photo.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleQuestion } from "@fortawesome/free-regular-svg-icons"
+
 
 export default function Photo({data}){
     return (
         <div className="photo-container">
-            <img className='photoOftheDay' src={data.hdurl} />
-            <div className="photo-Title">{data.title}</div>
+            <img className='photoOftheDay' src={data.hdurl} alt={data.title} />
+            <div className="photo-line-1">
+                <div className="photo-title">{data.title}</div>
+                <div className="photo-more">
+                    <FontAwesomeIcon icon={faCircleQuestion} />
+                </div>
+            </div>
+            <div className="photo-copyright">{data.copyright}</div>
         </div>
     )
 }

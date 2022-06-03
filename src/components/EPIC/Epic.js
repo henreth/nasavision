7428e62ea410bb264b9de313cbc3eab0b50fff45
Henry Miller <mail.henrym+git@gmail.com>
2022-06-02 22:49:28 -0400

    add epic photo of earth

---
+++ src/components/EPIC/Epic.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import "./Epic.css"
+
+export default function Epic({}){
+    let [earthPhoto,setEarthPhoto] = useState('')
+    
+    return (
+        <div>Test</div>
+    )
+}
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,23 +3,36 @@ import './HomePage.css';
 import apiKey from '../../Apikey';
 import Photo from '../Photo/Photo';
 import axios from 'axios';
+import Epic from '../EPIC/Epic';
 
-let nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey
+let photoOfTheDayURL = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey
+let epicURL = 'https://api.nasa.gov/EPIC/api/natural?api_key=' + apiKey
+
+let earthPhotoUrl = 'https://api.nasa.gov/EPIC/archive/natural/2022/06/01/png/epic_1b_20220601001751.png?api_key=' + apiKey
+// 
+// EXAMPLE: https://api.nasa.gov/EPIC/archive/natural/2022/06/01/png/epic_1b_20220601001751.png?api_key=DEMO_KEY
 
 export default function HomePage(){
     let [photoOfTheDay,setPhotoOfTheDay] = useState({});
 
+    let [epicData,setEpicData] = useState({})
+
     useEffect(()=>{
-        axios.get(nasaUrl)
+        axios.get(photoOfTheDayURL)
         .then(r=>{
-            console.log(r.data)
             setPhotoOfTheDay(r.data)})
+        
+        axios.get(epicURL)
+        .then(r=>{
+            setEpicData(r.data)
+        })
     },[])
 
     return(
         <React.Fragment>
             <h1 className='title'>NASAVISION</h1>
-            <Photo data={photoOfTheDay} />
+            {/* <Photo data={photoOfTheDay} /> */}
+            <Epic epicData ={epicData} />
             <div className='background-container'>
                 <div class="stars"></div>
                 <div class="twinkling"></div>
